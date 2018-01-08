460f0c73329d9b0ed766d5a14cbdb95a38b50d8b
arturohurtado <aqh829>
2018-01-08 16:32:24 -0600

    new update

--- src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -5,6 +5,7 @@ import org.launchcode.models.Menu;
 
 import javax.validation.constraints.NotNull;
 
+
 public class AddMenuItemForm {
 
     @NotNull
@@ -17,29 +18,42 @@ public class AddMenuItemForm {
 
     private Menu menu;
 
-    public AddMenuItemForm(){}
+    public AddMenuItemForm() {}
 
-    public AddMenuItemForm(Iterable<Cheese> cheeses, Menu menu){
-        this.menu = menu;
+    public AddMenuItemForm(Iterable<Cheese> cheeses, Menu menu) {
         this.cheeses = cheeses;
+        this.menu = menu;
     }
 
-    public int getMenuId(){return menuId;}
+    public int getMenuId() {
+        return menuId;
+    }
 
     public void setMenuId(int menuId) {
         this.menuId = menuId;
     }
 
-    public int getCheeseId() {return cheeseId;}
+    public int getCheeseId() {
+        return cheeseId;
+    }
 
     public void setCheeseId(int cheeseId) {
         this.cheeseId = cheeseId;
     }
 
-    public Iterable<Cheese> getCheeses(){return cheeses;}
+    public Iterable<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(Iterable<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
 
     public Menu getMenu() {
         return menu;
     }
 
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
 }
